e00fe5cadee3e95aceddb2446df481d77aa2b220
nec286 <peter.t.ludlow@gmail.com>
2016-05-27 11:10:46 +1000

    SITEPOINT CHANGES
    SP: Update Akismet
    SP: Refactor header to work with virtual dom
    SP: Customized .category__title
    SP: Customized .subcategories .badge-category
    SP: Google Analytics [new topic, new reply] via category
    SP: Tags
    SP: Updated hello-bar and flyout
    SP: Added Versioning to header
    SP: Updated Learnable links to Premium
    SP: Update header logo svg
    SP: Update header nav styles
    SP: Fix badge notification position
    SP: Fix header topic and notification positions
    SP: Update header nav (Add Shop, remove Deals, Store)
    SP: Update hello-bar accessibility
    SP: Add maestro-outlet, maestro-modal-once & Button styles
    SP: Add discourse-sp-api submodule
    SP: Add user preferences outlet
    SP: Add DemandBase tag
    SP: Update header nav (Add Podcasts, remove shop, versioning, reference)
    SP: Add after user details outlet
    SP: Add liveramp tracking to discourse-theme

--- app/serializers/admin_user_serializer.rb
+++ app/serializers/admin_user_serializer.rb
@@ -8,7 +8,9 @@ class AdminUserSerializer < AdminUserListSerializer
              :can_activate,
              :can_deactivate,
              :ip_address,
-             :registration_ip_address
+             :registration_ip_address,
+             :can_send_activation_email,
+             :custom_fields
 
   has_one :single_sign_on_record, serializer: SingleSignOnRecordSerializer, embed: :objects
 
